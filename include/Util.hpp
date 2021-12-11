38468b961c6cf6633ef27e60d7b71bdfa121fa0a
vis4rd <alagaesia.vrael@gmail.com>
2021-12-11 22:49:29 +0100

    added a missing function in imgui API called InputTextMultilineWithHint

---
+++ include/Util.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <imgui_internal.h>
+
+namespace ImGui
+{
+    // this is missing in imgui API, so have to add it myself
+    bool InputTextMultilineWithHint(
+        const char* label,
+        const char* hint,
+        char* buf,
+        std::size_t buf_size,
+        const ImVec2& size,
+        ImGuiInputTextFlags flags = 0,
+        ImGuiInputTextCallback callback = __null,
+        void* user_data = nullptr);
+};
---
+++ src/Util.cpp
@@ -0,0 +1,17 @@
+#include "../include/Util.hpp"
+
+namespace ImGui
+{
+    bool InputTextMultilineWithHint(
+        const char* label,
+        const char* hint,
+        char* buf,
+        std::size_t buf_size,
+        const ImVec2& size,
+        ImGuiInputTextFlags flags,
+        ImGuiInputTextCallback callback,
+        void* user_data)
+    {
+        return InputTextEx(label, hint, buf, (int)buf_size, size, flags | ImGuiInputTextFlags_Multiline, callback, user_data);
+    }
+};
