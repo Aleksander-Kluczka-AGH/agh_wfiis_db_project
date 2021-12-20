dde2eaff1ae5c32a117d1bb4a2047b6d8c364524
vis4rd <alagaesia.vrael@gmail.com>
2021-12-20 20:51:50 +0100

    added output for kierunek

--- src/FormOutputKierunek.cpp
+++ src/FormOutputKierunek.cpp
@@ -5,37 +5,13 @@ void formOutputKierunek()
     ImGui::TextColored(sf::Color{230, 230, 140}, "LISTA KIERUNKOW");
     ImGui::NewLine();
 
-    // try
-    // {
-    //     Connection C(
-    //         DATA::buf_host.getBuffer(),
-    //         DATA::buf_dbname.getBuffer(),
-    //         DATA::buf_user.getBuffer(),
-    //         DATA::buf_password.getBuffer());
-    //     // pqxx::connection C(buffer);
-    //     if(!C.check())
-    //     {
-    //         throw std::runtime_error("Connection is not open");
-    //     }
-    //     if(C.executeQuery("SELECT * FROM lab04.uczestnik"))
-    //     {
-    //         if(ImGui::BeginTable("##output_kierunek", 3, ImGuiTableFlags_Borders))
-    //         {
-    //             auto& res = C.getResult();
-    //             for(auto row : res)
-    //             {
-    //                 // ImGui::TableNextRow();
-    //                 ImGui::TableNextColumn(); ImGui::Text("%s", row["id_uczestnik"].c_str());
-    //                 ImGui::TableNextColumn(); ImGui::Text("%s", row["imie"].c_str());
-    //                 ImGui::TableNextColumn(); ImGui::Text("%s", row["nazwisko"].c_str());
-    //             }
-    //             ImGui::EndTable();
-    //         }
-    //     }
-    // }
-    // catch (const std::exception& e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    //     // return 1;
-    // }
+    std::stringstream str;
+    str << "host=" << DATA::buf_host
+        << " dbname=" << DATA::buf_dbname
+        << " user=" << DATA::buf_user
+        << " port=" << DATA::buf_port
+        << " password=" << DATA::buf_password;
+
+    tryConnect(str.str().c_str());
+    tableFromQuery("SELECT id_kierunek, nazwa, liczba_miejsc, dlugosc, stopien FROM prj.kierunek ORDER BY 1", {1, 2, 3, 4, 5});
 }
