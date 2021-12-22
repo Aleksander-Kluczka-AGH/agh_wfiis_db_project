fdb5a692c7f34bfa0aa23443d01d524795f320bf
vis4rd <alagaesia.vrael@gmail.com>
2021-12-22 17:45:35 +0100

    added professor list

--- src/FormOutputProwadzacy.cpp
+++ src/FormOutputProwadzacy.cpp
@@ -2,5 +2,17 @@
 
 void formOutputProwadzacy()
 {
-    ImGui::Text("Output Prowadzacy");
+    ImGui::TextColored(sf::Color{230, 230, 140}, "LISTA KIERUNKOW");
+    ImGui::NewLine();
+
+    std::stringstream str;
+    str << "host=" << DATA::buf_host
+        << " dbname=" << DATA::buf_dbname
+        << " user=" << DATA::buf_user
+        << " port=" << DATA::buf_port
+        << " password=" << DATA::buf_password;
+
+    tryConnect(str.str().c_str());
+    tableFromQuery("SELECT pr.id_prowadzacy, pr.imie, pr.drugie_imie, pr.nazwisko, pr.email, wy.nazwa AS wydzial, wy.skrot_nazwy AS skrot FROM prj.prowadzacy PR FULL JOIN prj.wydzial_prowadzacy USING(id_prowadzacy) FULL JOIN prj.wydzial WY USING (id_wydzial) ORDER BY 1;",
+        {1, 2, 4, 5, 6, 7});
 }
